c8ed41faf2bd7cdca1dd892040f1bf3a2cb202ba
Amy M <am.patoliya@patoliyainfotech.com>
2025-08-11 17:13:23 +0530

    Remove logs

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,10 +3,7 @@ import { register, login } from "../controllers/auth.controller";
 
 const router = Router();
 
-router.post('/register', (req, res, next) => {
-    console.log('Register route hit', req.body);
-    next();
-  }, register);
+router.post('/register', register);
 router.post('/login', login);
 
 export default router;

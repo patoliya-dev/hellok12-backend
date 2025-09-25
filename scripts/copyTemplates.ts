30f5e0e04096dd67634123ae31f49dafbd562c4f
Amy M <am.patoliya@patoliyainfotech.com>
2025-09-26 00:37:16 +0530

    Fix email template render issue

---
+++ scripts/copyTemplates.ts
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+import Logger from '../src/utils/winstonLogger.utils';
+
+const srcDir = path.join(process.cwd(), 'src/templates'); // always points to src/templates
+const distDir = path.join(process.cwd(), 'dist/templates');
+
+if (!fs.existsSync(distDir)) fs.mkdirSync(distDir, { recursive: true });
+
+fs.readdir(srcDir, (err, files) => {
+  if (err) {
+    Logger.error('Error reading templates folder:', err);
+    process.exit(1);
+  }
+
+  files.forEach(file => {
+    const srcFile = path.join(srcDir, file);
+    const distFile = path.join(distDir, file);
+
+    fs.copyFile(srcFile, distFile, err => {
+      if (err) Logger.error(`Failed to copy template ${file}:`, err);
+      else Logger.info(`Copied template: ${file}`);
+    });
+  });
+});
